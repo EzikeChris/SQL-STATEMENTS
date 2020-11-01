13e109c31c64799dc793dffdfb8b652d6be839cc
ezikechris <christophermbe1009@gmail.com>
2020-11-01 23:54:33 +0000

    initial commit

---
+++ database.js
@@ -0,0 +1,32 @@
+// collection of data
+// also contains a program of accessing and manipulatig data//
+// Data base managment system //
+ 
+
+
+// data base managment system
+// DBMS an application which enables us connect our app/program to an existing database//
+// talks to our database DBMS// 
+ 
+
+// EXAMPLES OF DATABASE MANAGMENT SYSTEM
+// Progames which lets us talk to our database are called DBMS THEY INLCUDE //
+// POSTGREsql MySql Oracle Database SQlite//
+// Mysql one of the most popular database managment system thereby we give commands to our DBMS which then interacts with the actuall database//
+
+
+// DataBase( a structured set of computerised data with an accessible interface)
+
+
+// DIFF BTW MYsql & SQL// 
+
+
+// SQL STRUCTURED QUERY LANGUAGE //
+// THE LA NGUAGE WE USE TO TALK T OUR DATA BASE E.G FIND ALL USERS WHO ARE 18YEARS OLD
+// WRITING LINES OF QUERIES SQL CODE AND IT GOES ON AND TALK TO OUR DATA BASE//
+
+// ALL RELATIONAL DATA BASES MAKE USE OF SEQUEL SQL EG THE ONCES LISTED ABOVE//
+
+// THE SQL STANDARD //
+
+// WHAT MAKES DBMS UNIQUE IS NOT THE SQL / LANGUAGE BUT THE FEATURES THE OFFER SQL WORKS ACROSS ALL LANGAUAGES USED BY  RELATIONAL DATABASES//
